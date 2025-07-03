afe89db37d65efd47d771e3eee0fad9261aa2259
Tyler Danger <tyler.pouchot@bigcommerce.com>
2025-07-03 08:03:47 -0500

    Update tailwind content paths for build server

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
+    path.resolve(__dirname, './src/**/*.{js,jsx,ts,tsx}'),
+    path.resolve(__dirname, './public/index.html'),
   ],
   theme: {
     extend: {},
